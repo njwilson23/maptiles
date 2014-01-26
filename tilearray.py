d3f4e3e82e3160a0098ccedcfce6f8a906fdcf94
Nat Wilson <njwilson23@gmail.com>
2014-01-26 09:41:32 -0500

    add get_extent method
    minor changes:
    - switched lat-lon covention to lon-lat
    - added dummy __getitem__

--- tilearray.py
+++ tilearray.py
@@ -3,7 +3,7 @@ OpenStreetMaps, Google Maps. """
 
 import os
 import math
-from math import pi
+from math import pi, floor
 import urllib2
 import cStringIO
 from PIL import Image
@@ -12,7 +12,7 @@ MAXLAT = math.atan(math.sinh(math.pi)) * 180.0 / math.pi
 
 class TileArray(object):
     """ TileArray is a class that handles getting online map tiles. Through
-    __getindex__ magic, the caller gets access to tiles using geographical
+    __getitem__ magic, the caller gets access to tiles using geographical
     coordinates.
     """
     def __init__(self, url, default_zoom=3):
@@ -20,20 +20,40 @@ class TileArray(object):
         self.default_zoom = default_zoom
         return
 
+    def __getitem__(self, key):
+        pass
+
+    def _getxy(self, coords, zoom):
+        n = 2**zoom
+        lat = coords[1] * pi / 180.0
+        lon = coords[0] * pi / 180.0
+        yp = math.asinh(math.tan(lat))
+        x = int(floor((1 + (lon / pi)) / 2 * n))
+        y = int(floor((1 - (yp / pi)) / 2 * n))
+        return x, y
+
+    def get_extents(self, coords, zoom):
+        """ Return the spatial extents of a tile containing *coordinates* at
+        zoom level *zoom*.
+        """
+        x, y = self._getxy(coords, zoom)
+        n = 2**zoom
+        minx = 360.0 / n * x - 180
+        maxx = 360.0 / n * (x+1) - 180
+        miny = pi * (1 - 2.0*y/n)
+        maxy = pi * (1 - 2.0*(y+1)/n)
+        iproj = lambda yp: math.atan(math.sinh(yp)) * 180 / pi
+        return (minx, maxx, iproj(miny), iproj(maxy))
+
     def get_tile_addr(self, coords, zoom=None):
         """ Given a lat/lon tuple (coords), return a tuple (zoom, col, row)
         for the tile address. """
         if zoom is None:
             zoom = self.default_zoom
-        if abs(coords[0]) > MAXLAT:
+        if abs(coords[1]) > MAXLAT:
             raise ValueError(u"Tile servers only contain tiles to {0}\u00b0 "
                              "latitude".format(round(MAXLAT, 2)))
-        n = 2**zoom
-        lat = coords[0] * pi / 180.0
-        lon = coords[1] * pi / 180.0
-        yp = math.asinh(math.tan(lat))
-        x = int(math.floor((1 + (lon / pi)) / 2 * n))
-        y = int(math.floor((1 - (yp / pi)) / 2 * n))
+        x, y = self._getxy(coords, zoom)
         return (zoom, x, y)
 
     def construct_addr(self, zoom, col, row):
@@ -50,13 +70,9 @@ class TileArray(object):
         return tile
 
     def get_tile(self, coords, zoom=None):
-        """ Return the tile corresponding to a lat-lon coordinate pair. """
+        """ Return the tile corresponding to a lon-lat coordinate pair. """
         tiletuple = self.get_tile_addr(coords, zoom)
         addr = self.construct_addr(*tiletuple)
         tile = self.download_tile(addr)
         return tile
-
-
-
-
 
